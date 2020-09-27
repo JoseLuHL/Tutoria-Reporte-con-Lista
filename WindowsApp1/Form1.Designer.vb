9a974ed370ceb32f172092375e5c284c8644e7f2
JOSELO <JOSELO@DESKTOP-26KMVBP>
2020-09-27 00:12:16 -0500

    Agregar archivos de proyecto.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CrystalReportViewer1 = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.CrystalReport11 = New WindowsApp1.CrystalReport1()
+        Me.SuspendLayout()
+        '
+        'CrystalReportViewer1
+        '
+        Me.CrystalReportViewer1.ActiveViewIndex = 0
+        Me.CrystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.CrystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.CrystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.CrystalReportViewer1.Location = New System.Drawing.Point(325, 0)
+        Me.CrystalReportViewer1.Name = "CrystalReportViewer1"
+        Me.CrystalReportViewer1.ReportSource = Me.CrystalReport11
+        Me.CrystalReportViewer1.Size = New System.Drawing.Size(672, 622)
+        Me.CrystalReportViewer1.TabIndex = 0
+        Me.CrystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(192, 226)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(127, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Mostrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 21
+        Me.ListBox1.Location = New System.Drawing.Point(12, 27)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(307, 193)
+        Me.ListBox1.TabIndex = 4
+        '
+        'ListBox2
+        '
+        Me.ListBox2.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 21
+        Me.ListBox2.Location = New System.Drawing.Point(12, 279)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(307, 151)
+        Me.ListBox2.TabIndex = 5
+        Me.ListBox2.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(997, 622)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CrystalReportViewer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CrystalReportViewer1 As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents CrystalReport11 As CrystalReport1
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+
+    Dim bd As JosephTutos_PedidosEntities
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        bd = New JosephTutos_PedidosEntities
+        Dim clientes = bd.cliente.ToList
+        For Each item In clientes
+            ListBox1.Items.Add(item.clie_nombre + " " + item.clie_apellidos)
+        Next
+
+    End Sub
+
+    Private Sub ListBox1_DoubleClick(sender As Object, e As EventArgs) Handles ListBox1.DoubleClick
+        If ListBox1.SelectedIndex > -1 Then
+            Dim nombre As String
+            nombre = ListBox1.SelectedItem
+            ListBox2.Items.Add(nombre)
+            ListBox2.Visible = True
+            ListBox1.ClearSelected()
+        End If
+    End Sub
+
+    Private Sub ListBox2_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox2.MouseDoubleClick
+        If ListBox2.SelectedIndex > -1 Then
+            ListBox2.Items.RemoveAt(ListBox2.SelectedIndex)
+        End If
+
+        If ListBox2.Items.Count <= 0 Then
+            ListBox2.Visible = False
+        End If
+        ListBox2.ClearSelected()
+    End Sub
+    Dim clientes As New List(Of cliente)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        clientes = New List(Of cliente)
+        Dim cli As New cliente
+        If ListBox2.Items.Count > 0 Then
+            For index = 0 To ListBox2.Items.Count - 1
+                cli = New cliente
+                cli.clie_nombre = ListBox2.Items(index)
+                cli.clie_id = index + 1
+                clientes.Add(cli)
+            Next
+        Else
+            For index = 0 To ListBox1.Items.Count - 1
+                cli = New cliente
+                cli.clie_nombre = ListBox1.Items(index)
+                cli.clie_id = index + 1
+                clientes.Add(cli)
+            Next
+        End If
+
+
+        Dim cry = New CrystalReport1
+        Me.CrystalReportViewer1.ReportSource = cry
+        cry.SetDataSource(clientes)
+        Me.CrystalReportViewer1.RefreshReport()
+        clientes = Nothing
+    End Sub
+End Class
